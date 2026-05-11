b2bca54066240862f9e0b60ef94f09cbc27cbc21
beloeser <bernardoloeseramaral@gmail.com>
2026-05-11 10:27:15 -0600

    acertos

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -25,7 +25,6 @@ export default function AppRoutes() {
       <Route path="/" element={<PublicRoute><Register /></PublicRoute>} />
       <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       <Route path="/visao-geral" element={<PrivateRoute><VisaoGeral /></PrivateRoute>} />
-      
       <Route path="/categories" element={<PrivateRoute><Categories /></PrivateRoute>} />
     </Routes>
   )
